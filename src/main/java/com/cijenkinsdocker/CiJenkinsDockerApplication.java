a4d92b2e0190aa16c352abaeaadca44a90e2de3e
Diego Lirio <ddamaceno@dellavolpe.com.br>
2017-04-07 17:29:19 -0300

    First Commit

---
+++ src/main/java/com/cijenkinsdocker/CiJenkinsDockerApplication.java
@@ -0,0 +1,12 @@
+package com.cijenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiJenkinsDockerApplication.class, args);
+	}
+}
