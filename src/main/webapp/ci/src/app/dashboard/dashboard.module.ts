55e3d3f9c65e4289ff93509a6e7e48ab4923f165
Felipe Bispo de Oliveira <fbispo@dellavolpe.com.br>
2017-04-10 17:12:10 -0300

    Foi colocado o routerLink para o direcionamento das rotas no html da dash

--- src/main/webapp/ci/src/app/dashboard/dashboard.module.ts
+++ src/main/webapp/ci/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashComponent } from './dash/dash.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { routing } from '../app.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+		routing
   ],
   declarations: [DashComponent],
   exports: [
